8b6fd0d208b78161d076d5ed0af78fa8b3e4f703
Vlad Zachary <vladzachary@Vlads-MacBook-Pro.local>
2013-09-14 18:01:47 -0400

    Adding stuff to new forms

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,25 @@
+class PicsController < ApplicationController
+	def new
+		# hello ruby code here
+		@pic = Pic.new
+	end
+
+	def index
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+end
